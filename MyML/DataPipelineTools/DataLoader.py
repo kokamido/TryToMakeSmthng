Add n-dim sphere optimization test for SGD, fix a few bugs around wrong broadcasting shown by test

MyML/DataPipelineTools/DataLoader.py
@@ -13,13 +13,6 @@ class DataLoader(ABC):
         """
         pass
 
-    @abstractmethod
-    def get_data_and_target_shapes(self) -> Tuple[Tuple[int], Tuple[int]]:
-        """
-        :return: (shape of single data item, shape of single target item)
-        """
-        pass
-
 
 class NumpyDataLoader(DataLoader):
     def __init__(self, data: np.ndarray, labels: np.ndarray, shuffle: bool = False):
@@ -31,11 +24,6 @@ class NumpyDataLoader(DataLoader):
         self.__data__ = data.copy()[permutation]
         self.__labels__ = np.array(labels.copy())[permutation]
         self.__current_index__ = 0
-        self.__data_shape__ = self.__data__[0].shape
-        self.__target_shape__ = self.__labels__[0].shape
-
-    def get_data_and_target_shapes(self) -> Tuple[Tuple[int], Tuple[int]]:
-        return self.__data_shape__, self.__target_shape__
 
     def get_data_batch(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray]:
         left = self.__current_index__
@@ -49,25 +37,12 @@ class NumpyDataLoader(DataLoader):
 
 
 class GeneratorBasedLoader(DataLoader):
-    """
-    Should bu used for test cases only
-    """
-
-    def __init__(
-        self, generator_func: Callable[[], Tuple[Any, Any]], calc_shapes_on_init=False
-    ):
+    def __init__(self, generator_func: Callable[[], Tuple[Any, Any]]):
+        """
+        :param generator_func:
+        :param calc_shapes_on_init:
+        """
         self.__generator_func__ = generator_func
-        self.__data_shape__ = None
-        self.__target_shape__ = None
-        if calc_shapes_on_init:
-            self.get_data_batch(1)
-
-    def get_data_and_target_shapes(self) -> Tuple[Tuple[int], Tuple[int]]:
-        if self.__data_shape__ is None or self.__target_shape__ is None:
-            raise Exception(
-                "Must call 'get_data_batch' method almost once before 'get_data_and_target_shapes' call"
-            )
-        return self.__data_shape__, self.__target_shape__
 
     def get_data_batch(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray]:
         data = []
@@ -76,8 +51,12 @@ class GeneratorBasedLoader(DataLoader):
             current_data, current_target = self.__generator_func__()
             data.append(current_data)
             target.append(current_target)
-        data_arr, target_arr = np.array(data), np.array(target)
-        if self.__target_shape__ is None or self.__data_shape__ is None:
-            self.__target_shape__ = (1,) if np.isscalar(target[0]) else target[0].shape
-            self.__data_shape__ = (1,) if np.isscalar(data[0]) else data[0].shape
-        return data_arr, target_arr
+        return np.array(data), np.array(target)
+
+
+class StubDataLoader(DataLoader):
+    def __init__(self):
+        self.__stub__ = np.array([0])
+
+    def get_data_batch(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray]:
+        return self.__stub__, self.__stub__

MyML/Helpers/TestHelpers/SingleDirectionGradsStub.py
@@ -19,7 +19,7 @@ class SingleDirectionGradsStub(CalcGraph):
         return self.__parameters__
 
     def calc_grads(self, data: np.ndarray) -> np.ndarray:
-        return (np.ones(self.__parameters__.shape) * self.__grad_value__)[None, :]
+        return np.ones(self.__parameters__.shape) * self.__grad_value__
 
     def calc_forward(self, data: np.ndarray) -> np.ndarray:
         return np.ones(data.shape[0]) * self.__forward_value__

MyML/Losses/IdenticalLoss.py
@@ -1,11 +1,11 @@
 from typing import Tuple
 
-from numpy import abs, ndarray, sign
+from numpy import ndarray
 
-from .AbstractLoss import LOSS_OUT, AbstractLoss
+from MyML.Losses.AbstractLoss import LOSS_OUT, AbstractLoss
 
 
-class AbsoluteError(AbstractLoss):
+class IdenticalLoss(AbstractLoss):
     def get_learnable_parameters(self) -> ndarray:
         """
         Absolute error loss has no learnable parameters"
@@ -18,13 +18,12 @@ class AbsoluteError(AbstractLoss):
         :param data: (actual_value, target_value), scalars
         :return: scalar
         """
-        actual_value, target_value = data
-        return sign(actual_value - target_value)
+        return 1
 
-    def calc_forward(self, data: Tuple[ndarray, ndarray]) -> LOSS_OUT:
+    def calc_forward(self, data: Tuple[LOSS_OUT, LOSS_OUT]) -> LOSS_OUT:
         """
         :param data: (actual_value, target_value), scalars
         :return: np.abs(actual_value - target_value)
         """
         actual_value, target_value = data
-        return abs(actual_value - target_value)
+        return actual_value

MyML/Optimizers/SGD.py
@@ -23,9 +23,7 @@ class SGDOptimizer(Optimizer):
             add_axis_if_1d, data.get_data_batch(self.__max_batch_size__)
         )
 
-        grad = np.squeeze(
-            np.apply_along_axis(graph_node.calc_grads, -1, data_to_calc_grads)
-        )
+        grad = np.apply_along_axis(graph_node.calc_grads, -1, data_to_calc_grads)
         grad = add_axis_if_1d(grad)
         if self.__norm_grad__:
             grad /= np.linalg.norm(grad)

MyML/Tests/Optimizers/SGD/test_SGD.py
@@ -3,10 +3,12 @@ import unittest
 import numpy as np
 
 from MyML.Algorithms.LinearRegression.LinearRegression import LinearRegression
-from MyML.DataPipelineTools.DataLoader import GeneratorBasedLoader
+from MyML.DataPipelineTools.DataLoader import GeneratorBasedLoader, StubDataLoader
 from MyML.Helpers.TestHelpers.ConstantLoss import ConstantLoss
+from MyML.Helpers.TestHelpers.OptimizationTestFunc import SphereFunc
 from MyML.Helpers.TestHelpers.SingleDirectionGradsStub import SingleDirectionGradsStub
 from MyML.Losses.AbsoluteError import AbsoluteError
+from MyML.Losses.IdenticalLoss import IdenticalLoss
 from MyML.Optimizers.SGD import SGDOptimizer
 
 
@@ -22,6 +24,17 @@ class SGDTests(unittest.TestCase):
             optimizer.update_node_parameters(node, loss, data_loader)
         np.testing.assert_array_almost_equal(parameters, [-1.0, -1.0, -1.0], 0.0001)
 
+    @staticmethod
+    def test_SGD_most_simple_case_big_batch():
+        node = SingleDirectionGradsStub(shape=[3], grad_value=2, forward_value=2)
+        loss = ConstantLoss(grads_value=1, loss_value=1)
+        optimizer = SGDOptimizer(0.01, 100)
+        data_loader = GeneratorBasedLoader(generator_func=lambda: (1, 1))
+        parameters = node.get_learnable_parameters()
+        for _ in range(100):
+            optimizer.update_node_parameters(node, loss, data_loader)
+        np.testing.assert_array_almost_equal(parameters, [-1.0, -1.0, -1.0], 0.0001)
+
     def test_SGD_random_data_shapes(self):
         for _ in range(50):
             shape = []
@@ -30,14 +43,33 @@ class SGDTests(unittest.TestCase):
             node = SingleDirectionGradsStub(shape=shape, grad_value=2, forward_value=2)
             loss = ConstantLoss(grads_value=1, loss_value=1)
             optimizer = SGDOptimizer(0.01, 1)
-            data_loader = GeneratorBasedLoader(
-                generator_func=lambda: (1, 1), calc_shapes_on_init=True
-            )
+            data_loader = GeneratorBasedLoader(generator_func=lambda: (1, 1))
             parameters = node.get_learnable_parameters()
             for _ in range(100):
                 optimizer.update_node_parameters(node, loss, data_loader)
             delta = np.sum(np.abs(parameters - np.ones(shape=shape) * (-1)).ravel())
-            self.assertAlmostEqual(delta, 0, delta=0.000001)
+            self.assertAlmostEqual(delta, 0, delta=1e-6)
+
+    def test_SGD_sphere_optimization(self):
+        for _ in range(10):
+            start_point = np.random.uniform(low=1.0, high=1.0, size=10)
+            end_point = np.zeros_like(start_point)
+            sphere_func = SphereFunc(start_point)
+            optimizer = SGDOptimizer(0.25, 1)
+            loss = IdenticalLoss()
+            data_loader = StubDataLoader()
+            point = sphere_func.get_learnable_parameters()
+            iter_num = 0
+            while np.sum(np.abs(point - end_point)) > 1e-2:
+                iter_num += 1
+                if iter_num == 20000:
+                    self.assertTrue(
+                        False,
+                        "Sphere minimum search with SGD takes too long time to converge",
+                    )
+                optimizer.update_node_parameters(sphere_func, loss, data_loader)
+                sphere_func.point = point
+            self.assertTrue(True)
 
     def test_SGD_with_linear_regression_1d_data(self):
         for _ in range(10):
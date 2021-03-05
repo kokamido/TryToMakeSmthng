7c3409cdd00bb27a664b94dd57e550937c1b9662
Alexander Pankratov <kokamidokokosian@gmail.com>
2021-03-05 13:38:03 +0500

    Fix Identical loss docstrings

--- MyML/Losses/IdenticalLoss.py
+++ MyML/Losses/IdenticalLoss.py
@@ -8,7 +8,7 @@ from MyML.Losses.AbstractLoss import AbstractLoss
 class IdenticalLoss(AbstractLoss):
     def get_learnable_parameters(self) -> ndarray:
         """
-        Absolute error loss has no learnable parameters"
+        Identical loss has no learnable parameters"
         :return:
         """
         raise NotImplementedError("Absolute error loss has no learnable parameters")
@@ -16,14 +16,14 @@ class IdenticalLoss(AbstractLoss):
     def calc_grads(self, data: Tuple[Any, Any]) -> float:
         """
         :param data: (actual_value, target_value), scalars
-        :return: scalar
+        :return: 1.0
         """
         return 1.0
 
     def calc_forward(self, data: Tuple[float, float]) -> float:
         """
         :param data: (actual_value, target_value), scalars
-        :return: np.abs(actual_value - target_value)
+        :return: actual_value
         """
         actual_value, target_value = data
         return actual_value
